35fb9a4533d36da5f2f68a42bf10182a505168a2
jorgehiler <andreshiler@gmail.com>
2022-02-27 18:57:48 -0500

    Spinner added

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr04_20212/lab1/ContactDataActivity.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr04_20212/lab1/ContactDataActivity.kt
@@ -1,4 +1,13 @@
 package co.edu.udea.compumovil.gr04_20212.lab1
 
-class ContactDataActivity {
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ContactDataActivity : AppCompatActivity() {
+
+    @SuppressLint("WrongViewCast")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 }
--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr04_20212/lab1/PersonalDataActivity.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr04_20212/lab1/PersonalDataActivity.kt
@@ -1,4 +1,21 @@
 package co.edu.udea.compumovil.gr04_20212.lab1
 
-class PersonalDataActivity {
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
+
+class PersonalDataActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_personal_data)
+
+        val spinner = findViewById<Spinner>(R.id.spnStudies)
+
+        val listStudies = listOf("Primaria", "Secundaria", "Universitaria", "Otro")
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, listStudies)
+        spinner.adapter = adapter
+    }
 }
