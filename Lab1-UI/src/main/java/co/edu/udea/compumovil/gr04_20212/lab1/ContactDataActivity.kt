f3f084e19350a8302cc8d3ef411a847fb72018cc
jonathans0ramirez <jonathan.ramirez@experimentality.co>
2022-02-28 17:02:57 -0500

    Cambio de layout y colores

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr04_20212/lab1/ContactDataActivity.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr04_20212/lab1/ContactDataActivity.kt
@@ -1,13 +1,20 @@
 package co.edu.udea.compumovil.gr04_20212.lab1
 
-import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class ContactDataActivity : AppCompatActivity() {
 
-    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_data)
+        setTitle(R.string.contact_data_title)
+        val buttonClick = findViewById<Button>(R.id.btnNextContactData)
+        buttonClick.setOnClickListener {
+            val intent = Intent(this, PersonalDataActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr04_20212/lab1/PersonalDataActivity.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr04_20212/lab1/PersonalDataActivity.kt
@@ -1,34 +1,69 @@
 package co.edu.udea.compumovil.gr04_20212.lab1
 
-import android.content.res.Configuration
+import android.app.DatePickerDialog
+import android.content.Context
+import android.content.Intent
+import android.content.res.Resources
 import android.os.Bundle
+import android.view.View
 import android.widget.ArrayAdapter
-import android.widget.LinearLayout
-import android.widget.Spinner
+import android.widget.AutoCompleteTextView
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class PersonalDataActivity : AppCompatActivity() {
+    var editText: EditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_personal_data)
+        setTitle(R.string.personal_data_title)
 
-        val spinner = findViewById<Spinner>(R.id.spnStudies)
+        val res: Resources = resources
+        val adapter: ArrayAdapter<String> = ArrayAdapter<String>(
+            this, R.layout.dropdown_item, res.getStringArray(R.array.scolarity_array)
+        )
+        val textView = findViewById<View>(R.id.scolarityDropdown) as AutoCompleteTextView
+        textView.setAdapter(adapter)
 
-        val listStudies = listOf("Primaria", "Secundaria", "Universitaria", "Otro")
+        editText = findViewById<View>(R.id.etBirthDate) as EditText
+        editText!!.transformIntoDatePicker(this, "dd/MM/yyyy")
 
-        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, listStudies)
-        spinner.adapter = adapter
+        val buttonClick = findViewById<Button>(R.id.btnNextPersonalData)
+        buttonClick.setOnClickListener {
+            val intent = Intent(this, ContactDataActivity::class.java)
+            startActivity(intent)
+        }
+    }
 
-        // Orientation
+    fun EditText.transformIntoDatePicker(context: Context, format: String, maxDate: Date? = null) {
+        isFocusableInTouchMode = false
+        isClickable = true
+        isFocusable = false
 
-        var linealLayoutName = findViewById<LinearLayout>(R.id.name_area)
-        if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            // landscape
-            linealLayoutName.orientation = LinearLayout.HORIZONTAL;
-        } else {
-            // portrait
-            linealLayoutName.orientation  = LinearLayout.VERTICAL;
+        val myCalendar = Calendar.getInstance()
+        val datePickerOnDataSetListener =
+            DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
+                myCalendar.set(Calendar.YEAR, year)
+                myCalendar.set(Calendar.MONTH, monthOfYear)
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                val sdf = SimpleDateFormat(format, Locale.UK)
+                setText(sdf.format(myCalendar.time))
+            }
+
+        setOnClickListener {
+            DatePickerDialog(
+                context, datePickerOnDataSetListener, myCalendar
+                    .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                myCalendar.get(Calendar.DAY_OF_MONTH)
+            ).run {
+                maxDate?.time?.also { datePicker.maxDate = it }
+                show()
+            }
         }
     }
 }
